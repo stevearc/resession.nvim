afabd1b2851f13271330b4812a339cc1e32aab9c
Steven Arcangeli <stevearc@stripe.com>
2023-05-03 22:19:14 -0700

    doc: add type annotation for extensions

--- lua/resession/types.lua
+++ lua/resession/types.lua
@@ -15,3 +15,11 @@
 ---@field reset nil|boolean|"auto" Close everthing before loading the session (default "auto")
 ---@field silence_errors nil|boolean Don't error when trying to load a missing session
 ---@field dir nil|string Name of directory to load from (overrides config.dir)
+
+---@class resession.Extension
+---@field on_save fun():any
+---@field on_load fun(data: any)
+---@field config nil|fun(options: table)
+---@field is_win_supported nil|fun(winid: integer, bufnr: integer): boolean
+---@field save_win nil|fun(winid: integer): any
+---@field load_win nil|fun(winid: integer, data: any): nil|integer
