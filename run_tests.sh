1a976b89af40be6bb90eb5f12e6abbc5a31bbef1
Steven Arcangeli <stevearc@stevearc.com>
2023-09-02 09:27:54 -0700

    ci: tweak run rules and makefile

--- run_tests.sh
+++ run_tests.sh
@@ -9,17 +9,16 @@ mkdir -p ".testenv/cache/nvim"
 PLUGINS=".testenv/data/nvim/site/pack/plugins/start"
 
 if [ ! -e "$PLUGINS/plenary.nvim" ]; then
-  git clone --depth=1 https://github.com/nvim-lua/plenary.nvim.git "$PLUGINS/plenary.nvim"
+	git clone --depth=1 https://github.com/nvim-lua/plenary.nvim.git "$PLUGINS/plenary.nvim"
 else
-  (cd "$PLUGINS/plenary.nvim" && git pull)
+	(cd "$PLUGINS/plenary.nvim" && git pull)
 fi
 
 XDG_CONFIG_HOME=".testenv/config" \
-  XDG_DATA_HOME=".testenv/data" \
-  XDG_STATE_HOME=".testenv/state" \
-  XDG_RUNTIME_DIR=".testenv/run" \
-  XDG_CACHE_HOME=".testenv/cache" \
-  nvim --headless -u tests/minimal_init.lua \
-  -c "TSUpdateSync" \
-  -c "PlenaryBustedDirectory ${1-tests} { minimal_init = './tests/minimal_init.lua' }"
+	XDG_DATA_HOME=".testenv/data" \
+	XDG_STATE_HOME=".testenv/state" \
+	XDG_RUNTIME_DIR=".testenv/run" \
+	XDG_CACHE_HOME=".testenv/cache" \
+	nvim --headless -u tests/minimal_init.lua \
+	-c "PlenaryBustedDirectory ${1-tests} { minimal_init = './tests/minimal_init.lua' }"
 echo "Success"
