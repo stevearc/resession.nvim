52e556bf9c32a74f2bc69e5cf984dd3899af597d
Steven Arcangeli <stevearc@stevearc.com>
2023-09-18 16:42:07 -0700

    types: make class types exact

--- lua/resession/types.lua
+++ lua/resession/types.lua
@@ -1,21 +1,21 @@
----@class resession.ListOpts
----@field dir nil|string Name of directory to save to (overrides config.dir)
+---@class (exact) resession.ListOpts
+---@field dir? string Name of directory to save to (overrides config.dir)
 
----@class resession.DeleteOpts
----@field dir nil|string Name of directory to save to (overrides config.dir)
+---@class (exact) resession.DeleteOpts
+---@field dir? string Name of directory to save to (overrides config.dir)
 
----@class resession.SaveOpts
----@field attach nil|boolean Stay attached to session after saving
----@field notify nil|boolean Notify on success
----@field dir nil|string Name of directory to save to (overrides config.dir)
+---@class (exact) resession.SaveOpts
+---@field attach? boolean Stay attached to session after saving
+---@field notify? boolean Notify on success
+---@field dir? string Name of directory to save to (overrides config.dir)
 
----@class resession.LoadOpts
----@field attach nil|boolean Attach to session after loading
----@field reset nil|boolean|"auto" Close everthing before loading the session (default "auto")
----@field silence_errors nil|boolean Don't error when trying to load a missing session
----@field dir nil|string Name of directory to load from (overrides config.dir)
+---@class (exact) resession.LoadOpts
+---@field attach? boolean Attach to session after loading
+---@field reset? boolean|"auto" Close everthing before loading the session (default "auto")
+---@field silence_errors? boolean Don't error when trying to load a missing session
+---@field dir? string Name of directory to load from (overrides config.dir)
 
----@class resession.Extension
+---@class (exact) resession.Extension
 ---@field on_save? fun():any
 ---@field on_pre_load? fun(data: any)
 ---@field on_post_load? fun(data: any)
@@ -24,6 +24,6 @@
 ---@field save_win? fun(winid: integer): any
 ---@field load_win? fun(winid: integer, data: any): nil|integer
 
----@class resession.SessionInfo
+---@class (exact) resession.SessionInfo
 ---@field name string Name of the current session
 ---@field dir string Name of the directory that the current session is saved in
